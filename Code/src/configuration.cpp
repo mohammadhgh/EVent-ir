739f9cbc4b2ccba2d3391035d1794bd57e0c0958
Mohammad  Ghasemi <mh90ghasemi@gmail.com>
2020-04-10 19:48:34 +0430

    Button, LED, Motor, ....

--- Code/src/configuration.cpp
+++ Code/src/configuration.cpp
@@ -11,45 +11,35 @@ PinConfiguration *PinConfiguration::getInstance()
 	return INSTANCE;
 }
 
-void static motorKeyWrapper()
-{
-	if (millis() - PinConfiguration::getInstance()->motorKeyTime > PinConfiguration::debounceDelay)
-	{
-		PinConfiguration::getInstance()->motorKeyTime = millis();
-		Motor::getInstance()->motorSwitch();
-	}
-}
-void static directionKeyWrapper()
-{
-	if (millis() - PinConfiguration::getInstance()->directionKeyTime > PinConfiguration::debounceDelay)
-	{
-		PinConfiguration::getInstance()->directionKeyTime = millis();
-		Motor::getInstance()->changeDirection();
-	}
-}
-
 void PinConfiguration::pinConfiguration()
 {
 	// Motor pins
 	pinMode(motorOut1, OUTPUT);
-	digitalWrite(motorOut1, INITIAL_POLARITY);
+	digitalWrite(motorOut1, LOW);
 	pinMode(motorOut2, OUTPUT);
-	digitalWrite(motorOut2, INITIAL_POLARITY);
+	digitalWrite(motorOut2, LOW);
 	pinMode(motorControl, OUTPUT);
 
-	//knobs
-	pinMode(motorSpeedKnob, INPUT);
+	//----knobs
+	pinMode(RR_knob_pin, INPUT);
 
-	//buttons
-	pinMode(motorKey, INPUT);
-	digitalWrite(motorKey, HIGH);
-	attachInterrupt(digitalPinToInterrupt(motorKey), motorKeyWrapper, FALLING);
+	//----buttons
+	pinMode(onButton_pin, INPUT);
+	digitalWrite(onButton_pin, HIGH);
+	// attachInterrupt(digitalPinToInterrupt(onButton_pin), onButton_handler, LOW);
 
-	pinMode(directionKey, INPUT);
-	digitalWrite(directionKey, HIGH);
-	attachInterrupt(digitalPinToInterrupt(directionKey), directionKeyWrapper, FALLING);
+	//----uSwitches
+	pinMode(open_uSw_pin, INPUT);
+	digitalWrite(open_uSw_pin, HIGH);
+
+	pinMode(close_uSw_pin, INPUT);
+	digitalWrite(close_uSw_pin, HIGH);
 
 	//----buzzer
 	pinMode(buzzerPin, OUTPUT);
 	digitalWrite(buzzerPin, HIGH);
+
+	//----LEDs
+	pinMode(gLED_pin, OUTPUT);
+	digitalWrite(gLED_pin, LOW);
 }
